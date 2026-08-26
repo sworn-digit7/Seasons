d9ade3b1d702bc882b88c6a7d24a067b127526ff
Gurleen Singh <singhgurleen551@gmail.com>
2026-08-26 21:38:47 +0100

    Add date difference calculation

--- seasons.py
+++ seasons.py
@@ -1,11 +1,23 @@
 from datetime import date
-
+import sys
 
 def main():
-    ...
+    dob = input("Date of birth: ")
+    minutes(dob)
 
 
-...
+def minutes(x):
+    
+    x = x.split("-")
+
+    today = date.today()
+    date1 = date(int(x[0]), int(x[1]), int(x[2]))
+
+    difference = today - date1
+
+    min = difference.days
+
+    print(min * 60)
 
 
 if __name__ == "__main__":
